8da56dcc07b14aedd92a398f3ee6ac63f31b555a
zeynep <sungjin@zeynep-mbp.local>
2019-03-12 15:33:39 -0400

    Updates

---
+++ precache-manifest.3957cfce002938aa4300d55958cdb67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  },
+  {
+    "revision": "d78f998af803ad02aec6",
+    "url": "/hello/static/js/main.7c4ab887.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/hello/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d78f998af803ad02aec6",
+    "url": "/hello/static/css/main.d4b09c14.chunk.css"
+  },
+  {
+    "revision": "25763b018ec8b8ad8c1b003aaa4e41f3",
+    "url": "/hello/index.html"
+  }
+];
